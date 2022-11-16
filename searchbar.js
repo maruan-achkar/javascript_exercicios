155770f1ebce8018da10e8819fe924b32839a063
maruan-achkar <maruan.achkar@catolicasc.edu.br>
2022-11-16 14:39:59 -0300

    js fix

--- searchbar.js
+++
@@ -1,2 +0,0 @@
-var results = ["Trocar Variaveis", "Celsius para Farenheit", "Farenheit para Celsius", "Area Circulo", "Par ou Impar", "Intervalo entre numeros", "Intervalo entre maior e menor de vetor", "Maior e menor numero", "Paisagem ou retrato", "Fizz ou Buzz", "String do objeto", "Par ou impar ate limite"]
-
