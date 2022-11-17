d649a49db6001e1e8abdf18fefac307cc7565de6
maruan-achkar <maruan.achkar@catolicasc.edu.br>
2022-11-16 22:37:09 -0300

    search bar now supports search!

---
+++ searchbar.js
@@ -0,0 +1,18 @@
+function search(){
+    var input, filter, dropdown, li, a, i, textvalue;
+    input = document.getElementById('searchbarinput');
+    filter = input.value.toUpperCase();
+    dropdown = document.getElementById("searchbardropdown");
+    li = dropdown.getElementsByTagName('li');
+
+    for(i=0; i < li.length; i++){
+        a = li[i].getElementsByTagName("a")[0];
+        textvalue = a.textContent || a.innerText;
+        if(textvalue.toUpperCase().indexOf(filter)>-1){
+            li[i].style.display = "";
+        }
+        else{
+            li[i].style.display = "none";
+        }
+    }
+}
