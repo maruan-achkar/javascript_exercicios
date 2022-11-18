0959cdb7f7167b44e6a7b6e2b9296e4b62d58b06
maruan-achkar <maruan.achkar@catolicasc.edu.br>
2022-11-18 19:36:22 -0300

    add "sem resultados" if serachbar has no results

--- searchbar.js
+++ searchbar.js
@@ -1,19 +1,22 @@
-function search(){
+function search() {
     var input, filter, dropdown, li, a, i, textvalue;
     input = document.getElementById('searchbarinput');
     filter = input.value.toUpperCase();
     dropdown = document.getElementById("searchbardropdown");
     li = dropdown.getElementsByTagName('li');
-    noresult = document.getElementById("searchbarnoresult");
-
-    for(i=0; i < li.length; i++){
-        a = li[i].getElementsByTagName("a")[0];
-        textvalue = a.textContent || a.innerText;
-        if(textvalue.toUpperCase().indexOf(filter)>-1){
-            li[i].style.display = "";
-        }
-        else{
-            li[i].style.display = "none";
-        }
+  
+    let matches = false;
+    for (i = 0; i < li.length; i++) {
+      if (li[i].dataset.noSearch !== undefined) continue
+      a = li[i].getElementsByTagName("a")[0];
+      textvalue = a.textContent || a.innerText;
+      if (textvalue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+        matches = true
+      } else {
+        li[i].style.display = "none";
+      }
     }
-}
+    if (!matches) dropdown.classList.add('noResults');
+    else dropdown.classList.remove('noResults');
+  }
