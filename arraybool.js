938ca438b1f830532c5b88c433d79e62cba20e15
maruan.achkar <MY_NAME@example.com>
2022-11-21 19:09:39 -0300

    fix array bool

--- arraybool.js
+++ arraybool.js
@@ -1,8 +1,8 @@
-let array = [false, 0, undefined, null, "", NaN];
+let array = [false, 0, undefined, null, "", NaN, 10, 'website'];
 
 function arraybool(){
     document.getElementById("arraybooloutput").innerHTML = "";
-    for(let i=0; i<=array.length; i++){
+    for(let i=0; i<array.length; i++){
         document.getElementById("arraybooloutput").innerHTML += "<b>" + array[i] + "</b>" + " = " + !!array[i] + "<br>";
     }
 }
